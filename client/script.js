c226be3634198ccd1833c9a80c8c21ca9d08f0ab
Animenkor <robin.engweiler@animenkor.ch>
2023-06-15 21:02:25 +0200

    first semi successful try

--- client/script.js
+++ client/script.js
@@ -1,18 +1,59 @@
 const socket = new WebSocket("ws://localhost:3000");
 
 socket.addEventListener("open", (event) => {
-  console.log("WebSocket connected!");
-  socket.send("Hello, server!");
+    console.log("WebSocket connected!");
+
+    // Generate a username on the first connection
+    const username = generateUsername();
+    document.getElementById("username").value = username;
+    socket.send(username);
 });
 
 socket.addEventListener("message", (event) => {
-  console.log(`Received message: ${event.data}`);
+    const messageObj = JSON.parse(event.data);
+    const messageContainer = document.getElementById("message-container");
+    const newMessage = document.createElement("div");
+    newMessage.innerText = messageObj;
+    messageContainer.appendChild(newMessage);
 });
 
 socket.addEventListener("close", (event) => {
-  console.log("WebSocket closed.");
+    console.log("WebSocket closed.");
 });
 
 socket.addEventListener("error", (event) => {
-  console.error("WebSocket error:", event);
+    console.error("WebSocket error:", event);
 });
+
+function sendMessage() {
+    const messageInput = document.getElementById("message-input");
+    const usernameInput = document.getElementById("username");
+
+    const message = messageInput.value;
+    const username = usernameInput.value;
+
+    if (message && username) {
+        const msg = {
+            type: "message",
+            message: {
+                username,
+                message,
+            },
+            sentAt: new Date().toLocaleTimeString(),
+        };
+        socket.send(JSON.stringify(msg));
+
+        messageInput.value = "";
+    }
+}
+
+function generateUsername() {
+    const adjectives = ["Happy", "Sad", "Excited", "Brave", "Clever", "Gentle", "Honest", "Kind", "Polite", "Silly"];
+
+    const nouns = ["Cat", "Dog", "Elephant", "Lion", "Tiger", "Monkey", "Penguin", "Dolphin", "Butterfly", "Owl"];
+
+    const adjective = adjectives[Math.floor(Math.random() * adjectives.length)];
+    const noun = nouns[Math.floor(Math.random() * nouns.length)];
+
+    return adjective + noun;
+}
--- server/websocketserver.js
+++ server/websocketserver.js
@@ -1,56 +1,66 @@
 const WebSocket = require("ws");
 const redis = require("redis");
-let publisher;
 
+let publisher;
 const clients = [];
 
-// Intiiate the websocket server
+// Initialize the websocket server
 const initializeWebsocketServer = async (server) => {
-  const client = redis.createClient({
-    socket: {
-      host: process.env.REDIS_HOST || "localhost",
-      port: process.env.REDIS_PORT || "6379",
-    },
-  });
-  // This is the subscriber part
-  const subscriber = client.duplicate();
-  await subscriber.connect();
-  // This is the publisher part
-  publisher = client.duplicate();
-  await publisher.connect();
+    const client = redis.createClient({
+        socket: {
+            host: process.env.REDIS_HOST || "localhost",
+            port: process.env.REDIS_PORT || "6379",
+        },
+    });
 
-  const websocketServer = new WebSocket.Server({ server });
-  websocketServer.on("connection", onConnection);
-  websocketServer.on("error", console.error);
-  await subscriber.subscribe("newMessage", onRedisMessage);
-  await publisher.publish("newMessage", "Hello from Redis!");
+    // This is the subscriber part
+    const subscriber = client.duplicate();
+    await subscriber.connect();
+
+    // This is the publisher part
+    publisher = client.duplicate();
+    await publisher.connect();
+
+    const websocketServer = new WebSocket.Server({ server });
+
+    websocketServer.on("connection", (ws) => onConnection(ws, subscriber));
+    websocketServer.on("error", console.error);
+
+    await subscriber.subscribe("newMessage", onRedisMessage);
+    await publisher.publish("newMessage", "Hello from Redis!");
 };
 
 // If a new connection is established, the onConnection function is called
-const onConnection = (ws) => {
-  console.log("New websocket connection");
-  ws.on("close", () => onClose(ws));
-  ws.on("message", (message) => onClientMessage(ws, message));
-  ws.send("Hello Client!");
-  //TODO!!!!!! Add the client to the clients array
+const onConnection = (ws, subscriber) => {
+    //console.log("New websocket connection");
+
+    ws.on("close", () => onClose(ws));
+    ws.on("message", (message) => onClientMessage(ws, message, subscriber));
+
+    ws.send("Hello Client!");
 };
 
 // If a new message is received, the onClientMessage function is called
-const onClientMessage = (ws, message) => {
-  console.log("Message received: " + message);
-  //TODO!!!!!! Send the message to the redis channel
+const onClientMessage = (ws, message, subscriber) => {
+    console.log("Message received: " + message);
+    publisher.publish("newMessage", message);
 };
 
 // If a new message from the redis channel is received, the onRedisMessage function is called
-const onRedisMessage = (message) => {
-  console.log("Message received: " + message);
-  //TODO!!!!!! Send the message to all connected clients
+const onRedisMessage = (channel, message) => {
+    //console.log("Message received from Redis: " + message);
+    clients.forEach((client) => {
+        client.send(message);
+    });
 };
 
 // If a connection is closed, the onClose function is called
 const onClose = (ws) => {
-  console.log("Websocket connection closed");
-  //TODO!!!!!! Remove the client from the clients array
+    //console.log("Websocket connection closed");
+    const index = clients.indexOf(ws);
+    if (index !== -1) {
+        clients.splice(index, 1);
+    }
 };
 
 module.exports = { initializeWebsocketServer };
